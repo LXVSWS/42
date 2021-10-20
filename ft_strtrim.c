a0e48f7702848a85b111aeee747a0cb9c47e45c7
lwyss <lwyss@student.42nice.fr>
2021-10-20 20:48:44 +0200

    save

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,14 +1,33 @@
 #include "libft/libft.h"
 
+int		isep(char char_of_s1, char *charset)
+{
+	int i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (char_of_s1 == charset[i++])
+			return (1);
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	(void)s1;
-	return (char *)(set);
+	int i;
+	char *s = NULL;
+
+	i = 0;
+	while (isep(s1[i], (char *)set))
+		i++;
+	printf("%i\n", i);
+	return (s);
 }
 
 int	main()
 {
 	char s1[] = "hahatesthaha";
-	char set[] = "haha";
+	char set[] = "ha";
 	printf("%s\n", ft_strtrim(s1, set));
 }
