f086c9cea0a93b7114c0da4dbc31712426a6aa9c
lwyss <lwyss@student.42nice.fr>
2021-10-25 15:29:50 +0200

    save

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: lwyss <lwyss@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 18:50:47 by lwyss             #+#    #+#             */
-/*   Updated: 2021/10/24 19:09:40 by lwyss            ###   ########.fr       */
+/*   Updated: 2021/10/25 15:24:35 by lwyss            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	(void)(*f);
-	(void)(*del);
-	return (lst);
+	t_list	*start;
+	t_list	*tmp;
+
+	if (!lst)
+		return (NULL);
+	start = ft_lstnew((*f)(lst->content));
+	lst = lst->next;
+	while (lst)
+	{
+		tmp = ft_lstnew((*f)(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&start, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&start, tmp);
+		lst = lst->next;
+	}
+	return (start);
 }
