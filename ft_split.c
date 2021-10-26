141d7f4019f5915cc44a225dd4f202f6a138f4ad
lwyss <lwyss@student.42nice.fr>
2021-10-26 13:43:17 +0200

    save

--- ft_split.c
+++ ft_split.c
@@ -1,16 +1,9 @@
 #include "libft/libft.h"
 
-static int	isep(char *s, char c)
+static int	isep(char c1, char c2)
 {
-	int		i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			return (1);
-		i++;
-	}
+	if (c1 == c2)
+		return (1);
 	return (0);
 }
 
@@ -22,24 +15,35 @@ char	**ft_split(char const *s, char c)
 	char	**pp;
 	char	*str;
 	int		i;
+	int		sep;
+	int		len;
 
 	i = 0;
-	pp = malloc(sizeof(char *) * 1 + 1);
-	while (!isep((char *)s, c))
+	sep = 0;
+	len = ft_strlen(s);
+	while (s[i])
+	{
+		if (isep(s[i], c))
+			sep++;
 		i++;
-	printf("%i\n", i);
-	str = malloc(sizeof(char) * i + 1);
-	str[i] = 0;
-	while (i)
-		str[i] = s[i];
-	*pp = str;
+	}
+	pp = malloc(sizeof(char) * (len - sep) + 1);
 	*(pp + 1) = NULL;
+	str = malloc(sizeof(char) * (len - sep) + 1);
+	str[len - sep] = 0;
+	i = 0;
+	while (!isep(s[i], c))
+	{
+		str[i] = s[i];
+		i++;		
+	}
+	*pp = str;
 	return (pp);
 }
 
 int	main()
 {
-	char s[] = "Hello";
+	char s[] = "Hellolhfbefehybflejhyfbsfl";
 	char c = 'l';
 	char **pp = ft_split(s, c);
 	printf("%s\n", *pp);
