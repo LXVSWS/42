ba2dd03b53b2cc4c83088cb9f431d4ba0602e1a8
lwyss <lwyss@student.42nice.fr>
2021-10-31 02:00:36 +0100

    save

--- test.c
+++ test.c
@@ -1,14 +1,29 @@
 #include "libft/libft.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 int	ft_printf(const char *s, ...)
 {
-	(void)s;
-	return (0);
+	va_list	ap;
+	char	*str;
+	int		i;
+
+	va_start(ap, s);
+	i = 0;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+	str = va_arg(ap, char *);
+	write(1, str, ft_strlen(str));
+	va_end(ap);
+	return (i);
 }
 
-int	main()
+int	main(int ac, char **av)
 {
-	ft_printf("my printf : ");
-	printf("\noriginal printf : ");
+	(void)ac;
+	ft_putnbr_fd(ft_printf("my printf : %s", av[1]), 1);
+	ft_putnbr_fd(printf("\noriginal printf : %s", av[1]), 1);
 }
