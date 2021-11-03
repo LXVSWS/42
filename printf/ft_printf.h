26a4a3a87f9c1251a54e4b4937312f617c3e8250
lwyss <lwyss@student.42nice.fr>
2021-11-03 23:31:00 +0100

    save

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwyss <lwyss@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 23:15:32 by lwyss             #+#    #+#             */
+/*   Updated: 2021/11/03 23:29:48 by lwyss            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -5,6 +17,8 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *s, ...);
+int	ft_getflag(char c, va_list ap);
+int	putsize(unsigned long long i, char *base, void *ptr);
 
 #endif
--- printf/ft_putsize.c
+++ printf/ft_putsize.c
@@ -1,35 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lwyss <lwyss@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/03 23:15:07 by lwyss             #+#    #+#             */
-/*   Updated: 2021/11/03 23:29:14 by lwyss            ###   ########.fr       */
+/*   Created: 2021/11/03 23:28:17 by lwyss             #+#    #+#             */
+/*   Updated: 2021/11/03 23:28:54 by lwyss            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "ft_printf.h"
 
-int	ft_printf(const char *s, ...)
+static void	putbase(unsigned long long nb, char *base)
 {
-	va_list			ap;
-	int				i;
-	int				size;
+	if (nb >= ft_strlen(base))
+		putbase(nb / ft_strlen(base), base);
+	ft_putchar_fd(base[nb % ft_strlen(base)], 1);
+}
 
-	va_start(ap, s);
-	i = 0;
+int	putsize(unsigned long long i, char *base, void *ptr)
+{
+	int	size;
+
+	if (ptr)
+		i = (unsigned long long)ptr;
 	size = 0;
-	while (s[i])
+	putbase(i, base);
+	if (i == 0)
+		return (1);
+	while (i)
 	{
-		if (s[i] == '%')
-			size += ft_getflag(s[++i], ap);
-		else
-			size += write(1, &s[i], 1);
-		i++;
+		i /= ft_strlen(base);
+		size++;
 	}
-	va_end(ap);
 	return (size);
 }
