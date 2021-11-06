0b8bf98a7bb15a0fb4a6e67e28e662f181ea8ead
lwyss <lwyss@student.42nice.fr>
2021-11-06 18:42:02 +0100

    save

--- tests.c
+++ tests.c
@@ -1,7 +1,17 @@
 #include "printf/ft_printf.h"
 #include <fcntl.h>
 
-static char	*ft_strjoin(char const *s1, char const *s2)
+int		ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
 	int		i;
@@ -9,15 +19,15 @@ static char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	if (!s2 || !s1)
+	if (!s2)
 		return (NULL);
 	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (str == NULL)
 		return (NULL);
-	while (s1[j])
+	while (s1 && s1[j])
 		str[i++] = s1[j++];
 	j = 0;
-	while (s2[j])
+	while (s2 && s2[j])
 		str[i++] = s2[j++];
 	str[i] = 0;
 	return (str);
@@ -25,13 +35,14 @@ static char	*ft_strjoin(char const *s1, char const *s2)
 
 char	*get_next_line(int fd)
 {
-    static char *s = "";
+    static char *s;
 	char *buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
     if (!buf)
         return (NULL);
     buf[BUFFER_SIZE] = 0;
-    while (read(fd, buf, BUFFER_SIZE))
-	    s = ft_strjoin(s, buf);
+	int i = 0;
+    read(fd, buf, BUFFER_SIZE);
+	s = ft_strjoin(s, buf);
     free(buf);
 	return (s);
 }
@@ -39,4 +50,4 @@ char	*get_next_line(int fd)
 int	main()
 {
 	ft_printf("%s", get_next_line(open("file.txt", O_RDONLY)));
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+}
