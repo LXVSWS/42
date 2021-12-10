b3c4d8babe31605ba0ceb6b65de1c8947837a139
lwyss <lwyss@student.42nice.fr>
2021-12-10 16:52:53 +0100

    save

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -1,10 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwyss <lwyss@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 15:10:26 by lwyss             #+#    #+#             */
+/*   Updated: 2021/12/10 15:33:07 by lwyss            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 
 size_t	strlen(char const *s);
 char	*itoa(int n);
+int		ft_atoi(const char *s);
 
 #endif
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,14 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwyss <lwyss@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 15:11:03 by lwyss             #+#    #+#             */
+/*   Updated: 2021/12/10 16:47:01 by lwyss            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
+
+void	handler(int signum)
+{
+	if (signum == SIGUSR1)
+		write(1, "!", 1);
+}
 
 int main(int ac, char **av)
 {
-    if (ac)
+	char	*pid;
+    if (ac == 1)
     {
-        char *pid = itoa(getpid());
+        pid = itoa(getpid());
         write(1, "PID : ", 6);
         write(1, pid, strlen(pid));
         write(1, "\n", 1);
-        free(pid);
+		signal(SIGUSR1, handler);
+        pause();
     }
     (void)av;
 }
