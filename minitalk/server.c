d57917624fa834f7aa50456016ddd6a15a841260
lwyss <lwyss@student.42nice.fr>
2021-12-12 19:29:50 +0100

    save

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: lwyss <lwyss@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 15:11:03 by lwyss             #+#    #+#             */
-/*   Updated: 2021/12/12 17:49:00 by lwyss            ###   ########.fr       */
+/*   Updated: 2021/12/12 19:28:58 by lwyss            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@
 
 void	handler(int signum)
 {
+	static int	flag = 0;
+
+	if (flag >= 8)
+	{
+		write(1, " ", 1);
+		flag = 0;
+	}
 	if (signum == SIGUSR1)
+	{
 		write(1, "0", 1);
-	else if (signum == SIGUSR2)
+		flag++;
+	}
+	if (signum == SIGUSR2)
+	{
 		write(1, "1", 1);
+		flag++;
+	}
 }
 
 int	main(void)
