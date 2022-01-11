e331cd79962a9891c7e54e9dd9b443d8599629f7
lwyss <lwyss@student.42nice.fr>
2022-01-11 19:40:11 +0100

    save

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,10 +1,10 @@
 #include "push_swap.h"
+#include <stdio.h>
 
 int main(int ac, char **av)
 {
 	char	**set;
 	int		i;
-	int		val;
 	t_list	*list;
 
     if (ac == 2)
@@ -12,14 +12,10 @@ int main(int ac, char **av)
 		set = ft_split(av[1], ' ');
 		i = -1;
 		while (set[++i])
-		{
-			val = ft_atoi(set[i]);
-			ft_lstadd_back(&list, ft_lstnew(&val));
-		}
+			ft_lstadd_back(&list, ft_lstnew(set[i]));
 		while (list)
 		{
-			write(1, list->content, 1);
-			write(1, "\n", 1);
+			printf("%i\n", ft_atoi(list->content));
 			list = list->next;
 		}
     }
