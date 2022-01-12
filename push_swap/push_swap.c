78ce3797a6ac9784ca17152152c9fe301b8d4ed6
lwyss <lwyss@student.42nice.fr>
2022-01-12 17:01:33 +0100

    save

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -4,18 +4,57 @@
 int main(int ac, char **av)
 {
 	char	**set;
+	int		**a;
+	int		*data;
 	int		i;
+	int		y;
 	t_list	*list;
 
     if (ac == 2)
     {
 		set = ft_split(av[1], ' ');
 		i = -1;
+		y = -1;
 		while (set[++i])
-			ft_lstadd_back(&list, ft_lstnew(set[i]));
+		{
+			while (set[i][++y])
+			{
+				if (set[i][y] < 48 || set[i][y] > 57)
+				{
+					write(2, "Error\n", 6);
+					free(set);
+					exit(1);
+				}
+			}
+			y = -1;
+		}
+		a = malloc(sizeof(int *) * i + 1);
+		a[i] = NULL;
+		i = -1;
+		y = -1;
+		while (set[++i])
+		{
+			data = malloc(sizeof(int));
+			*data = ft_atoi(set[i]);
+			/*
+			while (a[++y])
+			{
+				if (*(a[y]) == *data)
+				{
+					write(2, "Error\n", 6);
+					free(set);
+					free(a);
+					exit(1);
+				}
+			}
+			*/
+			a[i] = data;
+			ft_lstadd_back(&list, ft_lstnew(a[i]));
+		}
+		free(set);
 		while (list)
 		{
-			printf("%i\n", ft_atoi(list->content));
+			printf("%i\n", *(int *)(list->content));
 			list = list->next;
 		}
     }
