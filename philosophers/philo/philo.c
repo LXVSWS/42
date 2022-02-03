a42d2ec141ab8797f87bc4e759b6d7ddb6fe1de1
lwyss <lwyss@student.42nice.fr>
2022-02-03 19:36:55 +0100

    save

--- philosophers/philo/philo.c
+++ philosophers/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: lwyss <lwyss@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 15:57:26 by lwyss             #+#    #+#             */
-/*   Updated: 2022/02/03 16:35:43 by lwyss            ###   ########.fr       */
+/*   Updated: 2022/02/03 19:36:05 by lwyss            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ static void	*philo_routine(void *arg)
 	philo = (t_philo *)arg;
 	time_to_die = get_time() + (philo->data.time_to_die * 0.001);
 	printf("\033[44m%li %i is thinking\033[0m\n", get_time_ms(), philo->number);
+	pthread_mutex_lock(philo->left_fork);
+	pthread_mutex_lock(philo->right_fork);
+	printf("\033[44m%li %i has taken a fork\033[0m\n", get_time_ms(), philo->number);
+	printf("\033[44m%li %i has taken a fork\033[0m\n", get_time_ms(), philo->number);
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
 	while (get_time() < time_to_die)
 		;
 	printf("\033[41m%li %i died\033[0m\n", get_time_ms(), philo->number);
@@ -45,15 +51,18 @@ int	main(int ac, char **av)
 		{
 			philo[i].number = i + 1;
 			philo[i].data = data;
-			pthread_create(&thread_id[i], NULL, philo_routine, &philo[i]);
-			philo[i].thread_id = &thread_id[i];
+			pthread_create(&philo[i].thread_id, NULL, philo_routine, &philo[i]);
 			pthread_mutex_init(&fork[i], NULL);
-			philo[i].left_fork = fork[i];
+			philo[i].left_fork = &fork[i];
+			if (i != data.philo_total)
+				philo[i].right_fork = &fork[i + 1];
+			else
+				philo[i].right_fork = &fork[0];
 			usleep(1);
 		}
 		i = -1;
 		while (++i < data.philo_total)
-			pthread_join(thread_id[i], NULL);
+			pthread_join(philo[i].thread_id, NULL);
 		clean_exit(philo, thread_id, fork);
 	}
 	return (0);
--- philosophers/philo/philo.h
+++ philosophers/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: lwyss <lwyss@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 15:57:39 by lwyss             #+#    #+#             */
-/*   Updated: 2022/02/03 16:23:30 by lwyss            ###   ########.fr       */
+/*   Updated: 2022/02/03 18:48:05 by lwyss            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ typedef struct data
 typedef struct philo
 {
 	int				number;
-	pthread_t		*thread_id;
-	pthread_mutex_t	left_fork;	
-	pthread_mutex_t	right_fork;
+	pthread_t		thread_id;
+	pthread_mutex_t	*left_fork;	
+	pthread_mutex_t	*right_fork;
 	t_data			data;
 }	t_philo;
 
