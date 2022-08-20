1809a93bd12913d9d9d9b8c98438e693b6dc791f
LXV <lxvsws@gmail.com>
2022-08-20 13:06:01 +0200

    save

--- c++/m1/ex04/main.cpp
+++ c++/m1/ex04/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <fstream>
 
+int	replacing(std::string &buffer, std::string &s1, std::string &s2)
+{
+	int pos = buffer.find(s1);
+	if (pos == -1)
+		return (pos);
+	int i;
+	int j = 0;
+	for (i = 0 ; i < (int)s2.length(); i++)
+	{
+		if (i >= (int)s1.length())
+		{
+			std::string s3;
+			s3.append(s2, j);
+			buffer.insert(pos, s3);
+			break;
+		}
+		else
+			buffer[pos++] = s2[j++];
+	}
+	buffer.erase(pos, (int)s1.length() - i);
+	return (pos);
+}
+
 int main(int ac, char **av)
 {
 	if (ac == 4)
@@ -21,30 +44,12 @@ int main(int ac, char **av)
 			if (c != -1)
 				buffer.push_back(c);
 		}
+		input.close();
 		std::string s1 = av[2];
 		std::string s2 = av[3];
-		int pos = buffer.find(s1);
-		if (!pos)
-		{
-			std::cout << "Can't find occurence" << std::endl;
-			exit(-1);
-		}
-		int j = 0;
-		for (int i = 0 ; i < (int)s2.length(); i++)
-		{
-			if (i >= (int)s1.length())
-			{
-				std::string s3;
-				s3.append(s2, j);
-				buffer.insert(pos, s3);
-				break;
-			}
-			else
-				buffer[pos++] = s2[j++];
-		}
+		while (replacing(buffer, s1, s2) != -1);
 		output.open(std::string(av[1]) + std::string(".replace"));
 		output << buffer;
-		input.close();
 		output.close();
 	}
 	return (0);
