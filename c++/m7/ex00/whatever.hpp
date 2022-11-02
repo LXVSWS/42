40b0dc44a0c269c6096e8928f9b4ae46f578127b
lwyss <lwyss@student.42nice.fr>
2022-11-02 16:29:07 +0100

    save

--- c++/m7/ex00/whatever.hpp
+++ c++/m7/ex00/whatever.hpp
@@ -11,4 +11,20 @@ void swap(T &p1, T &p2)
 	p2 = tmp;
 }
 
+template<typename T>
+T min(T p1, T p2)
+{
+	if (p1 < p2)
+		return (p1);
+	return (p2);
+}
+
+template<typename T>
+T max(T p1, T p2)
+{
+	if (p1 > p2)
+		return (p1);
+	return (p2);
+}
+
 #endif
