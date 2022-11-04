6cb4efc07caae70f7d760d6239b5d2dee02a6f17
LXV <lxvsws@gmail.com>
2022-11-05 00:25:20 +0100

    save

--- c++/m7/ex02/Array.hpp
+++ c++/m7/ex02/Array.hpp
@@ -7,43 +7,43 @@ template<typename T>
 class Array
 {
 	T *array;
-	unsigned int size;
+	unsigned int _size;
 
 	public:
 		Array()
 		{
 			this->array = new T[0];
-			this->size = 0;
+			this->_size = 0;
 		}
 		Array(unsigned int n)
 		{
 			this->array = new T[n];
 			for (unsigned int i = 0 ; i < n ; i++)
 				this->array[i] = 0;
-			this->size = n;
+			this->_size = n;
 		}
 		Array(const Array &src)
 		{
-			this->array = new T[src.size];
-			for (unsigned int i = 0 ; i < src.size ; i++)
+			this->array = new T[src._size];
+			for (unsigned int i = 0 ; i < src._size ; i++)
 				this->array[i] = src.array[i];
-			this->size = src.size;
+			this->_size = src._size;
 		}
 		Array & operator=(const Array &src)
 		{
 			if (this != &src)
 			{
 				delete[] this->array;
-				this->array = new T[src.size];
-				for (unsigned int i = 0 ; i < src.size ; i++)
+				this->array = new T[src._size];
+				for (unsigned int i = 0 ; i < src._size ; i++)
 					this->array[i] = src.array[i];
-				this->size = src.size;
+				this->_size = src._size;
 			}
 			return (*this);
 		}
 		T & operator[](unsigned int i)
 		{
-			if (!this->size || i > this->size - 1)
+			if (!this->_size || i > this->_size - 1)
 				throw std::exception();
 			else
 				return (this->array[i]);
@@ -51,6 +51,11 @@ class Array
 		~Array()
 		{
 			delete[] this->array;
+		}
+
+		unsigned int size()
+		{
+			return (this->_size);
 		}
 };
 
--- c++/m7/ex02/main.cpp
+++ c++/m7/ex02/main.cpp
@@ -2,14 +2,42 @@
 
 int main()
 {
-	Array<int> test(1);
+	Array<int> test(2);
+	Array<int> test2(2);
 	try
 	{
-		std::cout << test[0] << std::endl;
+		for (unsigned int i = 0 ; i < test.size() ; i++)
+			test[i] = i;
+		std::cout << "First array: (size = " << test.size() << ")" << std::endl;
+		for (unsigned int i = 0 ; i < test.size() ; i++)
+			std::cout << test[i] << std::endl;
+		for (unsigned int i = 0 ; i < test2.size() ; i++)
+			test2[i] = i + 100;
+		std::cout << "Second array:" << std::endl;
+		for (unsigned int i = 0 ; i < test2.size() ; i++)
+			std::cout << test2[i] << std::endl;
+		test = test2;
+		std::cout << "First array after using assignment operator with second array as source:" << std::endl;
+		for (unsigned int i = 0 ; i < test.size() ; i++)
+			std::cout << test[i] << std::endl;
+		std::cout << "Modifying second array:" << std::endl;
+		for (unsigned int i = 0 ; i < test2.size() ; i++)
+			test2[i] = i + 200;
+		for (unsigned int i = 0 ; i < test2.size() ; i++)
+			std::cout << test2[i] << std::endl;
+		std::cout << "First array didn't change:" << std::endl;
+		for (unsigned int i = 0 ; i < test.size() ; i++)
+			std::cout << test[i] << std::endl;
+		Array<int> test3(test2);
+		std::cout << "Testing copy constructor with second array as parameter:" << std::endl;
+		for (unsigned int i = 0 ; i < test3.size() ; i++)
+			std::cout << test3[i] << std::endl;
+		std::cout << "Using incorrect index:" << std::endl;
+		std::cout << test[99] << std::endl;
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << "\nArray::InvalidIndexException" << std::endl;
+		std::cerr << "Array::InvalidIndexException" << std::endl;
 	}
 	return (0);
 }
