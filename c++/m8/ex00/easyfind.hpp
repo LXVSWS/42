8e919b453870c64bff426d9b856d581404b663c5
lwyss <lwyss@student.42nice.fr>
2022-11-05 17:59:30 +0100

    save

---
+++ c++/m8/ex00/easyfind.hpp
@@ -0,0 +1,13 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+template<typename T>
+int easyfind(T cont, int val)
+{
+	(void)cont;
+	return (val);
+}
+
+#endif
---
+++ c++/m8/ex00/main.cpp
@@ -0,0 +1,7 @@
+#include "easyfind.hpp"
+
+int main()
+{
+	std::cout << easyfind<int>(1, 1) << std::endl;
+	return (0);
+}
