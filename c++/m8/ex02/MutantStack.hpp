4151a73a0a61fdd47bb05d4dfa2c2b43a7adcb49
LXV <lxvsws@gmail.com>
2022-11-08 17:28:55 +0100

    save

---
+++ c++/m8/ex02/MutantStack.hpp
@@ -0,0 +1,13 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+
+};
+
+#endif
--- c++/m8/ex02/main.cpp
+++ c++/m8/ex02/main.cpp
@@ -1,4 +1,8 @@
+#include "MutantStack.hpp"
+
 int main()
 {
+	MutantStack<int> test;
+	std::cout << test.empty() << std::endl;
 	return (0);
 }
