dd901a0c79561d1e5c6c54cda5fd7d4e9331e0f0
LXV <lxvsws@gmail.com>
2022-12-01 09:36:45 +0100

    save

--- containers/main.cpp
+++ containers/main.cpp
@@ -2,8 +2,18 @@
 
 int main()
 {
-	std::vector<int> test1;
-	ft::vector<int> test2;
-	test2.reserve(3);
+	try
+	{
+		ft::vector<int> test;
+		std::cout << test << std::endl;
+		ft::vector<int> test2(10);
+		std::cout << test2 << std::endl;
+		test.reserve(10);
+		std::cout << test << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
--- containers/vector.hpp
+++ containers/vector.hpp
@@ -9,23 +9,76 @@ namespace ft
 	template< typename T, typename Alloc = std::allocator<T> >
 	class vector
 	{
-		private:
-			T *data;
-			Alloc allocator;
-			size_t original_size;
-
 		public:
-			vector() {}
+			typedef T value_type;
+			typedef Alloc allocator_type;
+			typedef typename allocator_type::reference reference;
+			typedef typename allocator_type::const_reference const_reference;
+			typedef typename allocator_type::pointer pointer;
+			typedef typename allocator_type::const_pointer const_pointer;
+			typedef ptrdiff_t difference_type;
+			typedef size_t size_type;
+
+			explicit vector(const allocator_type& alloc = allocator_type())
+			{
+				allocator = alloc;
+				data = allocator.allocate(1);
+				this->original_capacity = 1;
+				this->capacity = 1;
+			}
+			explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+			{
+				if (n)
+				{
+					allocator = alloc;
+					data = allocator.allocate(n);
+					this->original_capacity = n;
+					this->capacity = n;
+					for (size_t i = 0 ; i < n ; i++)
+						data[i] = val;
+				}
+				else
+				{
+					allocator = alloc;
+					data = allocator.allocate(1);
+					this->original_capacity = 1;
+					this->capacity = 1;
+				}
+			}
+			/*
+			template <class InputIterator>vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+			{
+
+			}
+			vector (const vector& x)
+			{
+
+			}
+			*/
 			~vector()
 			{
-				allocator.deallocate(data, original_size);
+				allocator.deallocate(data, this->original_capacity);
 			}
 			void reserve(size_t size)
 			{
-				original_size = size;
-				data = allocator.allocate(size);
+				if (size > this->capacity)
+				{
+					data = allocator.allocate(size);
+					this->capacity = size;
+				}
 			}
 
+		private:
+			T *data;
+			Alloc allocator;
+			size_t original_capacity;
+			size_t capacity;
+
+			friend std::ostream &operator<<(std::ostream& dst, const vector& src)
+			{
+				dst << src.capacity;
+				return (dst);
+			}
 	};
 }
 
