11254b1a29b7ac2dddf663ae887f2043c6e9fd24
LXV <lxvsws@gmail.com>
2022-12-05 05:37:20 +0100

    save

--- containers/iterator.hpp
+++ containers/iterator.hpp
@@ -5,8 +5,14 @@
 
 namespace ft
 {
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag {};
+	struct bidirectional_iterator_tag {};
+	struct random_access_iterator_tag {};
+
 	template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
-	class iterator
+	struct iterator
 	{
 		typedef T         value_type;
 		typedef Distance  difference_type;
@@ -15,11 +21,47 @@ namespace ft
 		typedef Category  iterator_category;
 	};
 
-	struct input_iterator_tag {};
-	struct output_iterator_tag {};
-	struct forward_iterator_tag {};
-	struct bidirectional_iterator_tag {};
-	struct random_access_iterator_tag {};
+	template <class Iterator>
+	struct iterator_traits
+	{
+		typedef typename Iterator::difference_type difference_type;
+		typedef typename Iterator::value_type value_type;
+		typedef typename Iterator::pointer pointer;
+		typedef typename Iterator::reference reference;
+		typedef typename Iterator::iterator_category iterator_category;
+	};
+
+	template <class T>
+	struct iterator_traits<T*>
+	{
+		typedef ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef T* pointer;
+		typedef T& reference;
+		typedef random_access_iterator_tag iterator_category;
+	};
+
+	template <class T>
+	struct iterator_traits<const T*>
+	{
+		typedef ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef const T* pointer;
+		typedef const T& reference;
+		typedef random_access_iterator_tag iterator_category;
+	};
+
+	template <class Iterator>
+	class reverse_iterator
+	{
+		public:
+			typedef Iterator iterator_type;
+			typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+			typedef typename iterator_traits<Iterator>::value_type value_type;
+			typedef typename iterator_traits<Iterator>::difference_type difference_type;
+			typedef typename iterator_traits<Iterator>::pointer pointer;
+			typedef typename iterator_traits<Iterator>::reference reference;
+	};
 
 	template <typename T>
 	class ConstIterator : public iterator<random_access_iterator_tag, const T>
@@ -50,26 +92,6 @@ namespace ft
 			bool operator==(const Iterator& src) const { return (ptr == src.ptr); }
 			bool operator!=(const Iterator& src) const { return (ptr != src.ptr); }
 			operator ConstIterator<T>() const { ConstIterator<T> src = ptr; return(src); }
-	};
-
-	template <class Iterator>
-	class iterator_traits
-	{
-	};
-
-	template <class T>
-	class iterator_traits<T*>
-	{
-	};
-
-	template <class T>
-	class iterator_traits<const T*>
-	{
-	};
-
-	template <class Iterator>
-	class reverse_iterator
-	{
 	};
 }
 
