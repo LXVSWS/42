10d74b0d5d44d1ee3f21308e0ec23f6126aef25c
LXV <lxvsws@gmail.com>
2023-01-04 02:33:37 +0100

    save

--- containers/map.hpp
+++ containers/map.hpp
@@ -11,10 +11,36 @@ namespace ft
 	class map
 	{
 		private:
-			Key *key_data;
-			T *value_data;
+			Compare compare;
+			Alloc allocator;
 		public:
-			map() { std::cout << "map created" << std::endl; }
+			typedef Key key_type;
+			typedef T mapped_type;
+			typedef ft::pair<const key_type, mapped_type> value_type;
+			typedef Compare key_compare;
+			//typedef ft::value_comp() value_compare;
+			typedef Alloc allocator_type;
+			typedef typename allocator_type::reference reference;
+			typedef typename allocator_type::const_reference const_reference;
+			typedef typename allocator_type::pointer pointer;
+			typedef typename allocator_type::const_pointer const_pointer;
+			/*
+			typedef ft::bidirectional_iterator<value_type> iterator;
+			typedef ft::bidirectional_iterator<const value_type> const_iterator;
+			typedef ft::reverse_iterator<iterator> reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+			*/
+			typedef ptrdiff_t difference_type;
+			typedef size_t size_type;
+
+			explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) \
+			: compare(comp), allocator(alloc) { std::cout << "Map created" << std::endl; }
+			/*
+			template <class InputIterator>
+			map(InputIterator first, InputIterator last, const key_compare& comp = key_compare(), \
+			const allocator_type& alloc = allocator_type()) {}
+			map(const map& x) {}
+			*/
 	};
 }
 
