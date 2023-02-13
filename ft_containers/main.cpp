86cfa8bc8dfb68fed1fda448f3a7b38df68dd419
LXV <lxvsws@gmail.com>
2023-02-14 00:10:24 +0100

    save

--- ft_containers/main.cpp
+++ ft_containers/main.cpp
@@ -10,9 +10,17 @@ int main()
 {
 	vector<int> t1(1, 42);
 
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
 	std::cout << t1.max_size() << std::endl;
 	for (vector<int>::iterator it = t1.begin() ; it != t1.end() ; ++it)
 		std::cout << *it << std::endl;
+
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::chrono::steady_clock::duration time = end - begin;
+	double ns = static_cast<double>(time.count()) * std::chrono::steady_clock::period::num / std::chrono::steady_clock::period::den;
+	std::cout.precision(9);
+	std::cout << std::fixed << ns << "s" << std::endl;
 
 	return (0);
 }
--- ft_containers/testing.sh
+++ ft_containers/testing.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
+WHITE='\033[1;37m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;93m'
 CYAN='\033[0;96m'
-WHITE='\033[1;37m'
+YELLOW='\033[0;93m'
 RESET='\033[0m'
 
 ft()
 {
-    cat main.cpp | sed 's/using namespace std/using namespace ft/g' | sed 's/namespace std/namespace ft/g' > maintmp.cpp
+    cat main.cpp | sed 's/using namespace std/using namespace ft/g' | sed 's/namespace std/namespace ft/g' > tmp.cpp
     rm main.cpp
-    mv maintmp.cpp main.cpp
+    mv tmp.cpp main.cpp
 }
 
 std()
 {
-    cat main.cpp | sed 's/using namespace ft/using namespace std/g' | sed 's/namespace ft/namespace std/g' > maintmp.cpp
+    cat main.cpp | sed 's/using namespace ft/using namespace std/g' | sed 's/namespace ft/namespace std/g' > tmp.cpp
     rm main.cpp
-    mv maintmp.cpp main.cpp
+    mv tmp.cpp main.cpp
 }
 
+clear
 clean=$(make fclean)
 make=$(make)
 std
 test=$(make test)
 ft
-printf "\n${CYAN}FT VERSION :${RESET}${WHITE}\n"
+printf "${WHITE}FT_CONTAINERS TESTER |\n_____________________|${RESET}\n"
+printf "\n${CYAN}FT VERSION :${RESET}\n"
 ./ft_containers
-printf "\n${YELLOW}STD VERSION :${RESET}${WHITE}\n"
+printf "\n${YELLOW}STD VERSION :${RESET}\n"
 ./std_containers
 diff=$(diff <(./ft_containers) <(./std_containers))
 if [ -z "$diff" ]
