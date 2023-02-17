61be32a2c525d825964c199f3e6b9644e36bb1d5
LXV <lxvsws@gmail.com>
2023-02-17 02:42:32 +0100

    save

--- ft_containers/main.cpp
+++ ft_containers/main.cpp
@@ -6,21 +6,46 @@ using namespace ft;
 #include <vector>
 #include <map>
 
-int main()
+void printTime(std::chrono::steady_clock::time_point begin, std::chrono::steady_clock::time_point end)
 {
-	vector<int> t1(1, 42);
-
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
-	std::cout << t1.max_size() << std::endl;
-	for (vector<int>::iterator it = t1.begin() ; it != t1.end() ; ++it)
-		std::cout << *it << std::endl;
-
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	std::chrono::steady_clock::duration time = end - begin;
 	double ns = static_cast<double>(time.count()) * std::chrono::steady_clock::period::num / std::chrono::steady_clock::period::den;
 	std::cout.precision(9);
 	std::cout << std::fixed << ns << "s" << std::endl;
+}
 
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "Please select a test" << std::endl;
+		return (1);
+	}
+	if (av[1][0] == '0' || av[1][0] == '1')
+	{
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		vector<int> t1(2, 42);
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == '0')
+			for (vector<int>::iterator it = t1.begin() ; it != t1.end() ; ++it)
+				std::cout << *it << std::endl;
+		if (av[1][0] == '1')
+			printTime(begin, end);
+		return (0);
+	}
+	if (av[1][0] == '2' || av[1][0] == '3')
+	{
+		vector<int> t1;
+		vector<int> t2(2, 42);
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		t1 = t2;
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == '2')
+			for (vector<int>::iterator it = t1.begin() ; it != t1.end() ; ++it)
+				std::cout << *it << std::endl;
+		if (av[1][0] == '3')
+			printTime(begin, end);
+		return (0);
+	}
 	return (0);
 }
--- ft_containers/testing.sh
+++ ft_containers/testing.sh
@@ -5,6 +5,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 CYAN='\033[0;96m'
 YELLOW='\033[0;93m'
+BLUE='\033[1;34m'
 RESET='\033[0m'
 
 ft()
@@ -28,14 +29,29 @@ std
 test=$(make test)
 ft
 printf "${WHITE}FT_CONTAINERS TESTER |\n_____________________|${RESET}\n"
-printf "\n${CYAN}FT VERSION :${RESET}\n"
-./ft_containers
-printf "\n${YELLOW}STD VERSION :${RESET}\n"
-./std_containers
-diff=$(diff <(./ft_containers) <(./std_containers))
+printf "\n${YELLOW}CONSTRUCTOR${RESET} | "
+diff=$(diff <(./ft_containers 0) <(./std_containers 0))
 if [ -z "$diff" ]
 then
-printf "\n${GREEN}DIFF OK${RESET} \xE2\x9C\x85 \n\n"
+printf "${GREEN}DIFF OK${RESET} \xE2\x9C\x85"
 else
-printf "\n${RED}DIFF KO${RESET} \xE2\x9D\x8C \n\n"
+printf "${RED}DIFF KO${RESET} \xE2\x9D\x8C"
 fi
+printf "\n${CYAN}FT TIMER : ${RESET}"
+./ft_containers 1
+printf "${BLUE}STD TIMER : ${RESET}"
+./std_containers 1
+sleep 1
+printf "\n${YELLOW}OPERATOR=${RESET} | "
+diff=$(diff <(./ft_containers 2) <(./std_containers 2))
+if [ -z "$diff" ]
+then
+printf "${GREEN}DIFF OK${RESET} \xE2\x9C\x85"
+else
+printf "${RED}DIFF KO${RESET} \xE2\x9D\x8C"
+fi
+printf "\n${CYAN}FT TIMER : ${RESET}"
+./ft_containers 3
+printf "${BLUE}STD TIMER : ${RESET}"
+./std_containers 3
+sleep 1
