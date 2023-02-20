30fe4ce57185e0a1bb5be686b89113dcbb7d25b7
LXV <lxvsws@gmail.com>
2023-02-20 01:39:24 +0100

    save

--- ft_containers/main.cpp
+++ ft_containers/main.cpp
@@ -16,15 +16,15 @@ void printTime(std::chrono::steady_clock::time_point begin, std::chrono::steady_
 
 int main(int ac, char **av)
 {
-	if (ac == 1)
+	if (ac < 2 || ac > 3)
 	{
-		std::cout << "Please select a test" << std::endl;
+		std::cout << "Please select a valid test" << std::endl;
 		return (1);
 	}
 	if (av[1][0] == '0' || av[1][0] == '1')
 	{
 		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-		vector<int> t1(2, 42);
+		vector<int> t1(10, 42);
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 		if (av[1][0] == '0')
 			for (vector<int>::iterator it = t1.begin() ; it != t1.end() ; ++it)
@@ -35,15 +35,106 @@ int main(int ac, char **av)
 	}
 	if (av[1][0] == '2' || av[1][0] == '3')
 	{
+		vector<int> t1(9, 66);
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		vector<int> t2(t1.begin(), t1.end());
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == '2')
+			for (vector<int>::iterator it = t2.begin() ; it != t2.end() ; ++it)
+				std::cout << *it << std::endl;
+		if (av[1][0] == '3')
+			printTime(begin, end);
+		return (0);
+	}
+	if (av[1][0] == '4' || av[1][0] == '5')
+	{
+		vector<int> t2(8, 6);
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		vector<int> t1(t2);
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == '4')
+			for (vector<int>::iterator it = t1.begin() ; it != t1.end() ; ++it)
+				std::cout << *it << std::endl;
+		if (av[1][0] == '5')
+			printTime(begin, end);
+		return (0);
+	}
+	if (av[1][0] == '6' || av[1][0] == '7')
+	{
 		vector<int> t1;
-		vector<int> t2(2, 42);
+		vector<int> t2(7, 96);
 		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 		t1 = t2;
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-		if (av[1][0] == '2')
+		if (av[1][0] == '6')
 			for (vector<int>::iterator it = t1.begin() ; it != t1.end() ; ++it)
 				std::cout << *it << std::endl;
-		if (av[1][0] == '3')
+		if (av[1][0] == '7')
+			printTime(begin, end);
+		return (0);
+	}
+	if (av[1][0] == '8' || av[1][0] == '9')
+	{
+		vector<int> t1(6, 22);
+		t1.push_back(1);t1.push_back(2);t1.push_back(3);t1.push_back(4);t1.push_back(5);
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		size_t res = t1.size();
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == '8')
+			std::cout << res << std::endl;
+		if (av[1][0] == '9')
+			printTime(begin, end);
+		return (0);
+	}
+	if (av[1][0] == 'a' || av[1][0] == 'b')
+	{
+		vector<int> t1;
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		size_t res = t1.max_size();
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == 'a')
+			std::cout << res << std::endl;
+		if (av[1][0] == 'b')
+			printTime(begin, end);
+		return (0);
+	}
+	if (av[1][0] == 'c' || av[1][0] == 'd')
+	{
+		vector<int> t1(4, 42);
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		t1.resize(2);
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == 'c')
+			for (vector<int>::iterator it = t1.begin() ; it != t1.end() ; ++it)
+				std::cout << *it << std::endl;
+		if (av[1][0] == 'd')
+			printTime(begin, end);
+		return (0);
+	}
+	if (av[1][0] == 'e' || av[1][0] == 'f')
+	{
+		vector<int> t1(8, 44);
+		t1.push_back(1);t1.push_back(2);t1.push_back(3);t1.push_back(4);t1.push_back(5);
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		size_t res = t1.capacity();
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == 'e')
+			std::cout << res << std::endl;
+		if (av[1][0] == 'f')
+			printTime(begin, end);
+		return (0);
+	}
+	if (av[1][0] == 'g' || av[1][0] == 'h')
+	{
+		vector<int> t1;
+		vector<int> t2(2, 24);
+		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+		size_t res = t1.empty();
+		size_t res2 = t2.empty();
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		if (av[1][0] == 'g')
+			std::cout << res << " " << res2 << std::endl;
+		if (av[1][0] == 'h')
 			printTime(begin, end);
 		return (0);
 	}
--- ft_containers/testing.sh
+++ ft_containers/testing.sh
@@ -23,35 +23,128 @@ std()
 }
 
 clear
+printf "${WHITE}FT_CONTAINERS TESTER |\n_____________________|\n\n${YELLOW}COMPILING...${RESET}"
 clean=$(make fclean)
 make=$(make)
 std
 test=$(make test)
 ft
-printf "${WHITE}FT_CONTAINERS TESTER |\n_____________________|${RESET}\n"
-printf "\n${YELLOW}CONSTRUCTOR${RESET} | "
+clear
+printf "${WHITE}FT_CONTAINERS TESTER |\n_____________________|\n\n${GREEN}COMPILATION OK \xE2\x9C\x85${RESET}\n\n"
+printf "${WHITE}• VECTOR TESTS${RESET}\n\n${YELLOW}CONSTRUCTOR${RESET} | "
 diff=$(diff <(./ft_containers 0) <(./std_containers 0))
 if [ -z "$diff" ]
 then
-printf "${GREEN}DIFF OK${RESET} \xE2\x9C\x85"
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
 else
-printf "${RED}DIFF KO${RESET} \xE2\x9D\x8C"
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
 fi
-printf "\n${CYAN}FT TIMER : ${RESET}"
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
 ./ft_containers 1
-printf "${BLUE}STD TIMER : ${RESET}"
+printf "${BLUE}STD EXEC TIME : ${RESET}"
 ./std_containers 1
 sleep 1
-printf "\n${YELLOW}OPERATOR=${RESET} | "
+printf "\n${YELLOW}CONSTRUCTOR (ITERATORS)${RESET} | "
 diff=$(diff <(./ft_containers 2) <(./std_containers 2))
 if [ -z "$diff" ]
 then
-printf "${GREEN}DIFF OK${RESET} \xE2\x9C\x85"
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
 else
-printf "${RED}DIFF KO${RESET} \xE2\x9D\x8C"
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
 fi
-printf "\n${CYAN}FT TIMER : ${RESET}"
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
 ./ft_containers 3
-printf "${BLUE}STD TIMER : ${RESET}"
+printf "${BLUE}STD EXEC TIME : ${RESET}"
 ./std_containers 3
+sleep 1
+printf "\n${YELLOW}COPY CONSTRUCTOR${RESET} | "
+diff=$(diff <(./ft_containers 4) <(./std_containers 4))
+if [ -z "$diff" ]
+then
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
+else
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
+fi
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
+./ft_containers 5
+printf "${BLUE}STD EXEC TIME : ${RESET}"
+./std_containers 5
+sleep 1
+printf "\n${YELLOW}OPERATOR= OVERLOADING${RESET} | "
+diff=$(diff <(./ft_containers 6) <(./std_containers 6))
+if [ -z "$diff" ]
+then
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
+else
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
+fi
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
+./ft_containers 7
+printf "${BLUE}STD EXEC TIME : ${RESET}"
+./std_containers 7
+sleep 1
+printf "\n${YELLOW}SIZE${RESET} | "
+diff=$(diff <(./ft_containers 8) <(./std_containers 8))
+if [ -z "$diff" ]
+then
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
+else
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
+fi
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
+./ft_containers 9
+printf "${BLUE}STD EXEC TIME : ${RESET}"
+./std_containers 9
+sleep 1
+printf "\n${YELLOW}MAX_SIZE${RESET} | "
+diff=$(diff <(./ft_containers a) <(./std_containers a))
+if [ -z "$diff" ]
+then
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
+else
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
+fi
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
+./ft_containers b
+printf "${BLUE}STD EXEC TIME : ${RESET}"
+./std_containers b
+sleep 1
+printf "\n${YELLOW}RESIZE${RESET} | "
+diff=$(diff <(./ft_containers c) <(./std_containers c))
+if [ -z "$diff" ]
+then
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
+else
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
+fi
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
+./ft_containers d
+printf "${BLUE}STD EXEC TIME : ${RESET}"
+./std_containers d
+sleep 1
+printf "\n${YELLOW}CAPACITY${RESET} | "
+diff=$(diff <(./ft_containers e) <(./std_containers e))
+if [ -z "$diff" ]
+then
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
+else
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
+fi
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
+./ft_containers f
+printf "${BLUE}STD EXEC TIME : ${RESET}"
+./std_containers f
+sleep 1
+printf "\n${YELLOW}EMPTY${RESET} | "
+diff=$(diff <(./ft_containers g) <(./std_containers g))
+if [ -z "$diff" ]
+then
+printf "${GREEN}OK${RESET} \xE2\x9C\x85"
+else
+printf "${RED}KO${RESET} \xE2\x9D\x8C"
+fi
+printf "\n${CYAN}FT EXEC TIME : ${RESET}"
+./ft_containers h
+printf "${BLUE}STD EXEC TIME : ${RESET}"
+./std_containers h
 sleep 1
