ba6be0156ded9dfdce33f1224a64b2c380a9968b
LXV <lxvsws@gmail.com>
2023-04-02 15:55:01 +0200

    save

--- ft_transcendence/src/app.module.ts
+++ ft_transcendence/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+	ServeStaticModule.forRoot({
+		rootPath: join(__dirname, '..', 'src'),
+		serveRoot: '/',
+	}),
+	ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
